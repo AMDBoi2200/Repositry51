ee57f89039a8eea18b5b6aa497eb48a6c40e1eb5
AMDBoi2200 <stevengu40@gmail.com>
2019-03-02 12:12:25 +0800

    update

--- L7/app.js
+++ L7/app.js
@@ -73,21 +73,20 @@ function update(){
 
     player.body.velocity.x = 0;
 
-    if(cursors.left.isDown){
-    player.body.velocity.x = -150;
-    player.animations.play('left');
-    }
+    // if(cursors.left.isDown){
+	   //  player.body.velocity.x = -150;
+	   //  player.animations.play('left');
+    // }
+    // else if(cursors.right.isDown){
+    //     player.body.velocity.x = 150;
+    // 	player.animations.play('right');
+    // }
+    // else{
+    // 	player.animations.stop();
+    // 	player.frame = 4;
+    // }
 
-    else if(cursors.right.isDown){
-        player.body.velocity.x = 150;
-    	player.animations.play('right');
-    }
-
-    else{
-    	player.animations.stop();
-    	player.frame = 4;
-    }
-    if(cursors.up.isDown && player.body.touching.down){
-    	player.body.velocity.y = -300;
-    }
+    // if(cursors.up.isDown && player.body.touching.down){
+    // 	player.body.velocity.y = -300;
+    // }
 }
